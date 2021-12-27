a4ed470e193977107c2c31da15f9f3ef814ee4b7
Desenvolvimento <Desenvolvimento@WPR-DEV002-002.econet.matriz.org>
2021-12-27 13:10:16 -0300

    increaseCounter/decreaseCounter methods on vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 0
   },
   mutations: {
+    increaseCounter(state) {
+      state.counter++
+   },
+   decreaseCounter(state) {
+      state.counter--
+   }
   },
   actions: {
   },
